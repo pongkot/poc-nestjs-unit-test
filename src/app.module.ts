17c9e05ebb08a2597e55ef1761c178861252eac2
Pongkot Sae-li <pongkot_sae@thaivivat.co.th>
2022-04-22 19:26:41 +0700

    feat: create bar service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FooModule } from './foo/foo.module';
+import { BarModule } from './bar/bar.module';
 
 @Module({
-  imports: [FooModule],
+  imports: [FooModule, BarModule],
   controllers: [AppController],
   providers: [AppService],
 })
