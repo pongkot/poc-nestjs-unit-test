6dbc174f2a6ce66cfbf5633f1a805509227ceead
Pongkot Sae-li <pongkot_sae@thaivivat.co.th>
2022-04-22 19:41:29 +0700

    feat: add test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BarModule } from './bar/bar.module';
+import { BarService } from './bar/bar.service';
+import { BazzModule } from './bazz/bazz.module';
+import { BazzService } from './bazz/bazz.service';
+import { FooModule } from './foo/foo.module';
 
 describe('AppController', () => {
   let appController: AppController;
+  let barService: BarService;
+  let bazzService: BazzService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [FooModule, BarModule, BazzModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    barService = app.get<BarService>(BarService);
+    bazzService = app.get<BazzService>(BazzService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! foo BAR BaZ"', () => {
+      jest.spyOn(barService, 'getBar').mockImplementation(() => 'BAR');
+      jest.spyOn(bazzService, 'getBazz').mockImplementation(() => 'BaZ');
+
+      expect(appController.getHello()).toBe('Hello World! foo BAR BaZ');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { BarService } from './bar/bar.service';
+import { BazzService } from './bazz/bazz.service';
+import { FooService } from './foo/foo.service';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly fooService: FooService,
+    private readonly barService: BarService,
+    private readonly bazzService: BazzService,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    const list = [
+      this.fooService.getFoo(),
+      this.barService.getBar(),
+      this.bazzService.getBazz(),
+    ];
+    return 'Hello World! ' + list.join(' ');
   }
 }
